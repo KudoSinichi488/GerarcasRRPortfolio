edbf09e36895d2d1eb85ebca065a3c388a2ec711
KudoSinichi488 <gerarcasrexrommel488@gmail.com>
2026-01-19 23:17:47 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,8 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 // Your Supabase project URL
 const supabaseUrl = 'https://dafbysgxzyjahljmtbyp.supabase.co'
 
-// Your anon public key (safe to use on the frontend)
+// Your anon public key (safe for frontend)
 const supabaseAnonKey = 'sb_publishable_OD0De1_0HAHkShU1nWNq6A_if_NgwvJ'
 
-// Create the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
